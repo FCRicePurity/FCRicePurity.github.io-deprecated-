344b53ba45e8d78b2f9ff6f993ac07e88e2a2a72
Ethan Duong <eduong100@gmail.com>
2022-05-02 15:25:50 -0700

    Navbar update + Externalize JS
    Move javascript functions into external JS file. Add link to github repository in navbar.

---
+++ fcpurity.js
@@ -0,0 +1,34 @@
+function giveScore() {
+    var element = document.getElementById("score");
+    var score = checkboxes();
+    element.innerHTML = "You scored " + score + "! Rating: " + roast(score);
+    element.scrollIntoView({ block: "center" });
+}
+function checkboxes() {
+    var inputElems = document.getElementsByTagName("input"),
+        count = 100;
+    for (var i = 0; i < inputElems.length; i++) {
+        if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
+            count--;
+        }
+    }
+    return count;
+}
+function roast(num) {
+    if (num == 100) {
+        return "Haven't touched a team yet.";
+    }
+    if (num > 85) {
+        return "Foundations virgin.";
+    }
+    if (num > 70) {
+        return "You've been on a team or two.";
+    }
+    if (num > 50) {
+        return "You live for Foundations.";
+    }
+    if (num > 25) {
+        return "Is getting this spicy allowed?";
+    }
+    return "Official Foundations whore.";
+}
