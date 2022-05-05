737ffbc2d756d93d75eca246dd77c54034d32168
Ethan Duong <eduong100@gmail.com>
2022-05-04 18:29:40 -0700

    Fix text-wrapping with checkboxes
    Change checkbox formatting to use bootstrap list-groups. Use flex format to center items and fix text-wrapping.

--- python_insert/insert.py
+++ python_insert/insert.py
@@ -5,4 +5,4 @@ lines = r_file.readlines()
 w_file = open('insert.html', 'w')
 
 for i in range(1,101):
-    w_file.write(f"<li><input type=\"checkbox\" id=\"{i}\"><label for=\"{i}\">" + lines[i-1].strip() + "</label></li>\n")
+    w_file.write(f"<li class=\"list-group-item d-flex justify-content-start align-items-start bg-transparent\"><label for=\"{i}\"><input type=\"checkbox\" id=\"{i}\">" + lines[i-1].strip() + "</label></li>\n")
